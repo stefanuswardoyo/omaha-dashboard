b013c7fc70a6baa95727138d0854568083abb8a5
Faisal Iqbal <faisaliqbal6633@gmail.com>
2024-03-19 16:06:47 +0500

    Mongo DB Fix

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,19 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    const connectionString: string | undefined = process.env.MONGODB_URI;
-    if (!connectionString) {
-      throw new Error("MongoDB connection string is not provided in environment variables");
-    }
-
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongoose.ConnectOptions);
+    await mongoose.connect(process.env.MONGODB_URI
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
