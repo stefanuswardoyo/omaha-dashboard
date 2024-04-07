4c6916159fe27e5e30488c55ffafdaf34dfb8432
Faisal Iqbal <faisaliqbal6633@gmail.com>
2024-04-08 00:20:37 +0500

    Fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import { Request, Response } from "express";
 import cors from "cors";
 import * as jwt from "jsonwebtoken";
 import connectDB from "./db";
